3e1196a6807127a2b90b04b6a95c92f8fcd46717
Claudio Lespier <claudiolespier@Claudios-Air.lan1>
2021-08-16 20:58:01 -0400

    Final Studio Completion

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById("container");
+            
+            for (let i = 0; i < json.length; i++){
+                let arrayPos = json[i]
+                let fullName = arrayPos.firstName + " " + arrayPos.lastName
+                let spaceHrs = arrayPos.hoursInSpace
+                let active = arrayPos.active
+                let skills = arrayPos.skills
+                let avatar = arrayPos.picture
+
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${fullName}</h3>
+                        <ul>
+                            <li>Hours in space: ${spaceHrs}</li>
+                            <li>Active: ${active}</li>
+                            <li>Skills: ${skills}</li>
+                        </ul>
+                </div>
+                <img class="avatar" src="${avatar}">
+                </div>`
+
+            };
+        });
+    });
+});
